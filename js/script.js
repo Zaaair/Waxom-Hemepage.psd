29c164b941284485345f6f7f497f4c08e292cb62
Slavik <slavikair2013@gmail.com>
2020-05-06 22:18:37 +0300

    add slaider

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // $('#jsBannerSlider').slick({
+    //     infinite: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    // });
+});
+
+$('jsBannerSlider').flickity()
